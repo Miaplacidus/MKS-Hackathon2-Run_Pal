39dab8a26518f3f7dfe1dc91ca9cb92b7650da38
Ifu Aniemeka <ianiemeka@gmail.com>
2014-05-08 15:43:58 -0500

    Edit use case file so that it's within the RunPal module

--- lib/run_pal/use_cases/create_circle.rb
+++ lib/run_pal/use_cases/create_circle.rb
@@ -1,33 +1,34 @@
-# module RunPal
-#   class CreateCircle < UseCase
+module RunPal
+  class CreateCircle < UseCase
 
-#     def run(inputs)
+    def run(inputs)
 
-#       user = RunPal.db.get_user(inputs[:admin_id])
-#       return failure (:user_does_not_exist) if user.nil?
+      user = RunPal.db.get_user(inputs[:admin_id])
+      return failure (:user_does_not_exist) if user.nil?
 
-#       circle = RunPal.db.get_circle(inputs[:circle_id])
-#       # task = TM.db.get_task(inputs[:task_id].to_i)
-#       # return failure(:task_does_not_exist) if task.nil?
+      circle = RunPal.db.get_circle(inputs[:circle_id])
+      # task = TM.db.get_task(inputs[:task_id].to_i)
+      # return failure(:task_does_not_exist) if task.nil?
 
-#       # emp = TM.db.get_emp(inputs[:employee_id].to_i)
-#       # return failure(:employee_does_not_exist) if emp.nil?
+      # emp = TM.db.get_emp(inputs[:employee_id].to_i)
+      # return failure(:employee_does_not_exist) if emp.nil?
 
-#       # proj = TM.db.get_project(task.projID)
-#       # return failure(:employee_not_assigned_to_proj) if proj.emp_ids[emp.id].nil?
+      # proj = TM.db.get_project(task.projID)
+      # return failure(:employee_not_assigned_to_proj) if proj.emp_ids[emp.id].nil?
 
-#       # add_employee_to_task(emp, task)
+      # add_employee_to_task(emp, task)
 
-#       # # Return a success with relevant data
-#       # success :task => task, :employee => emp
-#     end
+      # # Return a success with relevant data
+      # success :task => task, :employee => emp
+    end
 
-#     def create_circle(emp, task)
-#       # NOT SHOWN: Modify task to assign to employee
-#       # ...
-#       TM.db.update_task(task.id, {eid: emp.id})
-#       # {success?: true, task: task}
-#     end
+    def create_circle(emp, task)
+      # NOT SHOWN: Modify task to assign to employee
+      # ...
 
-#   end
-# end
+      # TM.db.update_task(task.id, {eid: emp.id})
+      # {success?: true, task: task}
+    end
+
+  end
+end
--- lib/use_case.rb
+++ lib/use_case.rb
@@ -3,7 +3,7 @@
 # This is needed for OpenStruct. Google "ruby openstruct" for more info
 require 'ostruct'
 
-module Timeline
+module RunPal
 
   class UseCase
     # Convenience method that lets us call `.run` directly on the class
