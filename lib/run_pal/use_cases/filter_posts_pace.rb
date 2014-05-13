9fc8b8c1bf2c3a600cf22977750760bb248e1746
Ifu Aniemeka <ianiemeka@gmail.com>
2014-05-12 23:30:14 -0500

    Clear database before each use case example; add use case and related spec for filtering posts by pace

--- lib/run_pal/use_cases/filter_posts_pace.rb
+++ lib/run_pal/use_cases/filter_posts_pace.rb
@@ -1,1 +1,15 @@
-# ifu
+module RunPal
+  class FilterPostsByPace < UseCase
+
+    def run(inputs)
+      post_arr = filter_posts_by_pace(inputs)
+
+      success :post_arr => post_arr
+    end
+
+    def filter_posts_by_pace(attrs)
+      RunPal.db.posts_filter_pace(attrs[:pace])
+    end
+
+  end
+end
