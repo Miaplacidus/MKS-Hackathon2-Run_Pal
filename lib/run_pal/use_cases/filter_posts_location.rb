582a995760e189b42629b9e37f1b5161c03a23cb
Ifu Aniemeka <ianiemeka@gmail.com>
2014-05-12 22:45:26 -0500

    Add filter posts by location use case and related spec

--- lib/run_pal/use_cases/filter_posts_location.rb
+++ lib/run_pal/use_cases/filter_posts_location.rb
@@ -1,1 +1,15 @@
-# ifu
+module RunPal
+  class FilterPostsByLocation < UseCase
+
+    def run(inputs)
+      post_arr = filter_posts_by_location(inputs)
+
+      success :post_arr => post_arr
+    end
+
+    def filter_posts_by_location(attrs)
+      RunPal.db.posts_filter_location(attrs[:location], attrs[:radius])
+    end
+
+  end
+end
