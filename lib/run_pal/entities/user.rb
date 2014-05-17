338789845bf7c111e5f060a108f556c7c82df019
Ifu Aniemeka <ianiemeka@gmail.com>
2014-05-17 13:20:25 -0500

    Partially integrate OmniAuth into application

--- lib/run_pal/entities/user.rb
+++ lib/run_pal/entities/user.rb
@@ -2,6 +2,8 @@ module RunPal
   class User < Entity
     attr_accessor :id, :username, :gender, :email, :bday, :history, :password
     attr_accessor :rating, :wallet_id, :circle_ids, :level
+
+    # consider image
     # circle_ids: array
 
     validates_presence_of :username, :gender, :email, :bday, :password
